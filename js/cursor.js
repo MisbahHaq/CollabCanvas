7e3abed3c5156c990f8bee3f5f1f5411fab5dab8
Misbah Haque <misbahmuneer2003@gmail.com>
2025-09-23 16:19:29 +0500

    Update

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-// Cursor reveal and follow on mouse move
-
-document.addEventListener('mousemove', function(e) {
-  const cursorContainer = document.getElementById('CursorContainerID');
-
-  cursorContainer.style.left = e.clientX + 'px';
-  cursorContainer.style.top = e.clientY + 'px';
-  cursorContainer.style.opacity = 1;
-});
