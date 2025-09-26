41ef73024f8fb799f751bdc10575d0cc15d240d5
Misbah Haque <misbahmuneer2003@gmail.com>
2025-09-26 16:10:58 +0500

    Organization Setup

---
+++ board/app/(dashboard)/_components/sidebar/list.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { OrganizationList, useOrganizationList } from "@clerk/nextjs";
+
+export const List = () => {
+    const { userMemberships } = useOrganizationList({
+        userMemberships: {
+            infinite: true,
+        },
+    });
+
+    if (!userMemberships.data?.length) return null;
+
+    return (
+        <ul>
+            {userMemberships.data?.map((mem) => (
+                <p key={mem.organization.id}>{mem.organization.name}</p>
+            ))}
+        </ul>
+    );
+};
