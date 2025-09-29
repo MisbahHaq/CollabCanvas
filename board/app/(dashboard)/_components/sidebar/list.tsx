56936f69d33b67e2a5c3fba3d228e18a107c1b52
Misbah Haque <misbahmuneer2003@gmail.com>
2025-09-29 10:30:04 +0500

    Organization Logo

--- board/app/(dashboard)/_components/sidebar/list.tsx
+++ board/app/(dashboard)/_components/sidebar/list.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { OrganizationList, useOrganizationList } from "@clerk/nextjs";
+import { Item } from "./item";
 
 export const List = () => {
     const { userMemberships } = useOrganizationList({
@@ -14,7 +15,12 @@ export const List = () => {
     return (
         <ul>
             {userMemberships.data?.map((mem) => (
-                <p key={mem.organization.id}>{mem.organization.name}</p>
+                <Item
+                    key={mem.organization.id}
+                    id={mem.organization.id}
+                    name={mem.organization.name}
+                    imageUrl={mem.organization.imageUrl}
+                />
             ))}
         </ul>
     );
