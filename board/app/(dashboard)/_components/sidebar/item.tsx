6377a8ba85ba0d8df1b0f224c21497c2722ea6d4
Misbah Haque <misbahmuneer2003@gmail.com>
2025-09-29 12:43:24 +0500

    Dashboard Button Hover Update

--- board/app/(dashboard)/_components/sidebar/item.tsx
+++ board/app/(dashboard)/_components/sidebar/item.tsx
@@ -3,18 +3,15 @@
 import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps {
     id: string;
     name: string;
     imageUrl: string;
-};
+}
 
-export const Item = ({
-    id,
-    name,
-    imageUrl,
-}: ItemProps) => {
+export const Item = ({ id, name, imageUrl }: ItemProps) => {
     const { organization } = useOrganization();
     const { setActive } = useOrganizationList();
 
@@ -28,15 +25,18 @@ export const Item = ({
 
     return (
         <div className="aspect-square relative">
-            <Image
-                fill
-                alt={name}
-                src={imageUrl}
-                onClick={onClick}
-                className={cn(
-                    "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-                    isActive && "opacity-100"
-                )} />
+            <Hint label={name} side="right" align="start" sideOffset={20}>
+                <Image
+                    fill
+                    alt={name}
+                    src={imageUrl}
+                    onClick={onClick}
+                    className={cn(
+                        "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+                        isActive && "opacity-100"
+                    )}
+                />
+            </Hint>
         </div>
     );
 };
