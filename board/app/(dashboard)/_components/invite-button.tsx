9a8cfb0fbe8be19fdeb063c768638a2da64fa7f8
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-02 15:33:17 +0500

    Update

--- board/app/(dashboard)/_components/invite-button.tsx
+++ board/app/(dashboard)/_components/invite-button.tsx
@@ -1,7 +1,7 @@
 import { Plus } from "lucide-react";
 import { OrganizationProfile } from "@clerk/nextjs";
 
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
 export const InviteButton = () => {
@@ -14,6 +14,7 @@ export const InviteButton = () => {
                 </Button>
             </DialogTrigger>
             <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+                <DialogTitle className="sr-only">Organization Profile</DialogTitle>
                 <OrganizationProfile
                 />
             </DialogContent>
