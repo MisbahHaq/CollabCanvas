c451f4fd42784ca6f689077d039397a31c578b20
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-02 15:32:46 +0500

    Dashboard Page Setup

--- board/app/(dashboard)/page.tsx
+++ board/app/(dashboard)/page.tsx
@@ -1,6 +1,28 @@
+"use client";
+import { useOrganization } from "@clerk/nextjs";
+import { useSearchParams } from "next/navigation";
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
 const DashboardPage = () => {
+    const { organization } = useOrganization();
+    const searchParams = useSearchParams();
+    const search = searchParams.get("search");
+    const favorites = searchParams.get("favorites");
+
+    const query = {
+        search: search || undefined,
+        favorites: favorites || undefined,
+    };
+
     return (
-        <div>Dashboard Root Page</div>
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {!organization ? <EmptyOrg /> : (
+                <BoardList
+                    orgId={organization.id}
+                    query={query} />
+            )}
+        </div>
     );
 };
 
