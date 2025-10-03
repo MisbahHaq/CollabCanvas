d023851f2a3ff99de5bc484a15111afb8ff4164d
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-03 12:20:04 +0500

    Update

--- board/app/(dashboard)/layout.tsx
+++ board/app/(dashboard)/layout.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import { Navbar } from "./_components/navbar";
 import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
+import { useSidebar } from "@/hooks/use-sidebar";
+import { cn } from "@/lib/utils";
 
 interface DashboardLayoutProps {
     children: React.ReactNode;
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+    const { isOpen, close } = useSidebar();
+
     return <main className="h-full">
+        {/* Mobile backdrop */}
+        {isOpen && (
+            <div
+                className="fixed inset-0 bg-black/50 z-[0] lg:hidden"
+                onClick={close}
+            />
+        )}
         <Sidebar />
-        <div className="pl-[60px] h-full">
+        <div className={cn(
+            "h-full transition-all duration-300 ease-in-out",
+            "lg:pl-[60px]",
+            isOpen ? "pl-[60px]" : "pl-0 lg:pl-[60px]"
+        )}>
             <div className="flex gap-x-3 h-full">
                 <OrgSidebar />
                 <div className="h-full flex-1">
