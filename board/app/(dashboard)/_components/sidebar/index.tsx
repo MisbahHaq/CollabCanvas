86abe7f8b0f060e5e0faa568b9294aa9f18e0a1d
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-03 12:20:19 +0500

    Update

--- board/app/(dashboard)/_components/sidebar/index.tsx
+++ board/app/(dashboard)/_components/sidebar/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { List } from "./list";
 import { NewButton } from "./new-button";
+import { useSidebar } from "@/hooks/use-sidebar";
+import { cn } from "@/lib/utils";
 
 export const Sidebar = () => {
+    const { isOpen, close } = useSidebar();
+
     return (
-        <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-            <List />
+        <aside className={cn(
+            "fixed z-[2] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white transition-transform duration-300 ease-in-out",
+            "lg:translate-x-0",
+            isOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"
+        )}>
+            <List onItemClick={close} />
             <NewButton />
         </aside>
     );
