542005a9f72dbe652062c94d98e39ff59e93c2b9
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-03 12:20:00 +0500

    Update

--- board/app/(dashboard)/page.tsx
+++ board/app/(dashboard)/page.tsx
@@ -16,7 +16,7 @@ const DashboardPage = () => {
     };
 
     return (
-        <div className="flex-1 h-[calc(100%-80px)] p-6">
+        <div className="flex-1 h-[calc(100%-80px)] p-4 md:p-6">
             {!organization ? <EmptyOrg /> : (
                 <BoardList
                     orgId={organization.id}
