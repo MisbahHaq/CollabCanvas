84db882bb8b86333f0c8632a30861d7f884d457b
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-06 15:54:31 +0500

    New Board Update

--- board/app/(dashboard)/_components/new-board-button.tsx
+++ board/app/(dashboard)/_components/new-board-button.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
 import { useApiMutation } from "@/hooks/use-api-mutation";
+import { useRouter } from "next/navigation";
 
 interface NewBoardButtonProps {
     orgId: string;
@@ -11,25 +12,28 @@ interface NewBoardButtonProps {
 }
 
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
-
+    const router = useRouter();
     const { mutate, pending } = useApiMutation(api.board.create);
 
     const onClick = () => {
         mutate({
             orgId,
-            title: "Untitled"
-        }).then((id) => {
-            toast.success("Board created");
-            // TODO: Redirect to /board/S
-        }).catch(() => toast.error("Failed to create board"));
-    }
+            title: "Untitled",
+        })
+            .then((id) => {
+                toast.success("Board created");
+                router.push(`/board/${id}`);
+            })
+            .catch(() => toast.error("Failed to create board"));
+    };
     return (
         <button
             disabled={pending || disabled}
             onClick={onClick}
             className={cn(
                 "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
-                (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+                (pending || disabled) &&
+                "opacity-75 hover:bg-blue-600 cursor-not-allowed"
             )}
         >
             <div />
