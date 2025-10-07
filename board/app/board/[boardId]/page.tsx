5b1fc506c5aefa0d2232674f82b677a280178324
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-07 11:31:16 +0500

    Page Update

--- board/app/board/[boardId]/page.tsx
+++ board/app/board/[boardId]/page.tsx
@@ -8,12 +8,13 @@ interface BoardIdPageProps {
     };
 };
 
-const BoardIdPage = ({
+const BoardIdPage = async ({
     params,
 }: BoardIdPageProps) => {
+    const { boardId } = await params;
     return (
-        <Room roomId={params.boardId} fallback={<Loading />}>
-            <Canvas boardId={params.boardId} />
+        <Room roomId={boardId} fallback={<Loading />}>
+            <Canvas boardId={boardId} />
         </Room>
     );
 };
