bff4f43c2cc8e86380784cc556c8257164a1a652
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-07 10:08:43 +0500

    Toolbar Update

--- board/app/board/[boardId]/_components/toolbar.tsx
+++ board/app/board/[boardId]/_components/toolbar.tsx
@@ -1,3 +1,5 @@
+
+
 export const Toolbar = () => {
     return (
         <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
@@ -16,5 +18,11 @@ export const Toolbar = () => {
                 </div>
             </div>
         </div>
+    );
+};
+
+Toolbar.Skeleton = function ToolbarSkeleton() {
+    return (
+        <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4 bg-white h-[360px] w-[52px] shadow-md rounded-md" />
     );
 };
