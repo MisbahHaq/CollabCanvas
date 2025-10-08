a82dc892fa346a05d30a8a6dfd2923b97c805ee1
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-08 17:26:12 +0500

    Canvas Update

--- board/app/board/[boardId]/_components/canvas.tsx
+++ board/app/board/[boardId]/_components/canvas.tsx
@@ -1,24 +1,57 @@
 "use client";
 
-import { useState } from "react";
-import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useState, useCallback } from "react";
+import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { useHistory, useCanUndo, useCanRedo } from "@/liveblocks.config";
+import {
+    useHistory,
+    useCanUndo,
+    useCanRedo,
+    useMutation,
+} from "@/liveblocks.config";
+import { CursorsPresence } from "./cursors-presence";
+import { pointerEventToCanvasPoint } from "@/lib/utils";
 
 interface CanvasProps {
     boardId: string;
-};
+}
 
-export const Canvas = ({ boardId, }: CanvasProps) => {
+export const Canvas = ({ boardId }: CanvasProps) => {
     const [canvasState, setCanvasState] = useState<CanvasState>({
         mode: CanvasMode.None,
     });
 
+    const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 });
+
     const history = useHistory();
     const canUndo = useCanUndo();
     const canRedo = useCanRedo();
+
+    const onWheel = useCallback((e: React.WheelEvent) => {
+        setCamera((camera) => ({
+            x: camera.x - e.deltaX,
+            y: camera.y - e.deltaY,
+        }));
+    }, []);
+
+    const onPointerLeave = useMutation((
+        { setMyPresence }
+    ) => {
+        setMyPresence({ cursor: null });
+    }, []);
+
+    const onPointerMove = useMutation(
+        ({ setMyPresence }, e: React.PointerEvent) => {
+            e.preventDefault();
+
+            const current = pointerEventToCanvasPoint(e, camera);
+
+            setMyPresence({ cursor: current });
+        },
+        []
+    );
 
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
@@ -32,6 +65,14 @@ export const Canvas = ({ boardId, }: CanvasProps) => {
                 undo={history.undo}
                 redo={history.redo}
             />
+            <svg className="h-[100vh] w-[100vw]"
+                onWheel={onWheel}
+                onPointerMove={onPointerMove}
+                onPointerLeave={onPointerLeave}>
+                <g>
+                    <CursorsPresence />
+                </g>
+            </svg>
         </main>
     );
 };
