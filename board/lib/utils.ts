4c4fee0336e5a00d3cd88cd750706105ef1f1f22
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-10 18:22:22 +0500

    Utils Update

--- board/lib/utils.ts
+++ board/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { Camera, Color, Layer, Point, Side, XYWH } from "@/types/canvas";
+import { Camera, Color, Layer, LayerType, PathLayer, Point, Side, XYWH } from "@/types/canvas";
 
 const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
@@ -17,8 +17,8 @@ export function pointerEventToCanvasPoint(
   camera: Camera
 ) {
   return {
-    x: Math.round(e.clientX) - camera.x,
-    y: Math.round(e.clientY) - camera.y,
+    x: e.clientX - camera.x,
+    y: e.clientY - camera.y,
   };
 }
 
@@ -91,10 +91,73 @@ export function findIntersectingLayersWithRectangle(
     }
   }
   return ids;
-};
+}
 
 export function getContrastingTextColor(color: Color) {
   const luminance = 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
 
   return luminance > 182 ? "black" : "white";
+}
+
+export function penPointsToPathLayer(
+  points: number[][],
+  color: Color
+): PathLayer {
+  if (points.length < 2) {
+    throw new Error("Cannot transform points with less than 2 points");
+  }
+
+  let left = Number.POSITIVE_INFINITY;
+  let top = Number.POSITIVE_INFINITY;
+  let right = Number.NEGATIVE_INFINITY;
+  let bottom = Number.NEGATIVE_INFINITY;
+
+  for (const point of points) {
+    const [x, y] = point;
+
+    if (left > x) {
+      left = x;
+    }
+    if (top > y) {
+      top = y;
+    }
+    if (right < x) {
+      right = x;
+    }
+    if (bottom < y) {
+      bottom = y;
+    }
+  }
+
+  const width = right - left;
+  const height = bottom - top;
+
+  return {
+    type: LayerType.Path,
+    x: left + width / 2,
+    y: top + height / 2,
+    width,
+    height,
+    fill: color,
+    points: points.map(([x,y,pressure])=> [x - left - width / 2, y - top - height / 2, pressure]),
+  };
+};
+
+export function getSvgPathFromStroke(stroke: number[][]) {
+  if (!stroke.length) return "";
+
+  const d = stroke.reduce(
+    (acc, [x, y], i) => {
+      if (i === 0) {
+        acc.push("M", x.toString(), y.toString());
+      } else {
+        acc.push("L", x.toString(), y.toString());
+      }
+      return acc;
+    },
+    [] as string[]
+  );
+
+  d.push("Z");
+  return d.join(" ");
 };
