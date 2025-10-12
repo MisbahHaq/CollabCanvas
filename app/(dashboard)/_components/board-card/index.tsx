116ea528687c3b98416bd2441a17a4a3d9b154c0
Misbah Haque <misbahmuneer2003@gmail.com>
2025-10-12 19:41:39 +0500

    Index Update

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -61,10 +61,10 @@ export const BoardCard = ({
         <Link href={`/board/${id}`}>
             <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
                 <div className="relative flex-1 bg-amber-50 bg-cover bg-center bg-no-repeat">
-                    <Image src={imageUrl} alt={title} fill className=" object-fit" />
+                    <Image src={imageUrl} alt={title} fill className="object-fit" />
                     <Overlay />
                     <Actions id={id} title={title} side="right">
-                        <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+                        <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none md:opacity-0 md:group-hover:opacity-100">
                             <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
                         </button>
                     </Actions>
